8cdc2dacb51a257dec9128dd7b9a8eef68245954
Nikita <khegay.nik@gmail.com>
2024-01-29 18:27:05 +0600

    first commit

--- api/router/__init__.py
+++ api/router/__init__.py
@@ -4,3 +4,4 @@ from api.src.endpoints import auth
 router = APIRouter()
 
 router.include_router(auth.router, prefix="/auth", tags=["auth"])
+router.include_router(auth.router, prefix="/goods", tags=["goods"])
